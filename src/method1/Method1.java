7b31dd2778fc2288e7d5cbd9777e85959e015bba
soumyaaaaaaaaaaaaaaaaaaaaaaa <167617017+soumyaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-05-15 14:59:59 +0530

    string method1

---
+++ src/method1/Method1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package method1;
+
+/**
+ *
+ * @author dell1
+ */
+public class Method1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+   
+        String str=new String("netbeans");
+        
+        /*
+        //System.out.println(str.length());
+        int len=str.length();
+        System.out.println(len);*/
+        
+        
+        /*String str2=str.toUpperCase();
+        System.out.println(str2);*/
+        
+        
+        /*str=str.trim();
+        System.out.println(str);*/
+        
+        /*String str2=str.substring(3);
+        System.out.println(str2);*/
+        
+        String str2=str.replace('e', 'M');
+        //System.out.println(str2);
+        
+        System.out.println(str2+ " " +str);
+        
+    }
+    
+}
+    
